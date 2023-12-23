af2286a93ca25c8b6d2cd0cdd8f6260755bba202
eulegang <eulegang@eulegang.dev>
2023-12-23 00:00:51 -0600

    Adding forwarder

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,98 @@
 // Uncomment this block to pass the first stage
 // use std::net::UdpSocket;
 
-use std::net::Ipv4Addr;
+use std::net::{Ipv4Addr, SocketAddrV4};
 
+use clap::Parser;
 use tokio::net::UdpSocket;
 
 use fmt::Bincode;
 
 mod fmt;
 
+#[derive(Parser)]
+struct Cli {
+    #[clap(long)]
+    resolver: Option<SocketAddrV4>,
+}
+
 #[tokio::main]
 async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
+    let cli = Cli::parse();
+
     let udp_socket = UdpSocket::bind("127.0.0.1:2053")
         .await
         .expect("Failed to bind to address");
+
+    let forwarder = if let Some(addr) = cli.resolver {
+        let sock = UdpSocket::bind("0.0.0.0:0")
+            .await
+            .expect("failed to bind forwarder to address");
+        sock.connect(addr)
+            .await
+            .expect("could not connect udp socket");
+
+        Some(sock)
+    } else {
+        None
+    };
 
     let mut in_buf = [0; 512];
     let mut out_buf = Vec::with_capacity(512);
 
     'conn: loop {
-        match udp_socket.recv_from(&mut in_buf).await {
+        let (bytes, source) = match udp_socket.recv_from(&mut in_buf).await {
             Ok((size, source)) => {
                 eprintln!("Received {} bytes from {}", size, source);
-                let bytes = &in_buf[0..size];
-
-                let packet = match fmt::Packet::decode(bytes) {
-                    Ok((_, packet)) => packet,
-
-                    Err(e) => {
-                        eprintln!("malformed request header: {e:?}");
-
-                        continue 'conn;
-                    }
-                };
-
-                let packet = transform(packet);
-
-                packet.encode(&mut out_buf);
-
-                udp_socket
-                    .send_to(&out_buf, source)
-                    .await
-                    .expect("Failed to send response");
-
-                out_buf.clear();
+                (&in_buf[0..size], source)
             }
             Err(e) => {
                 eprintln!("Error receiving data: {}", e);
                 break;
             }
+        };
+
+        let packet = match fmt::Packet::decode(bytes) {
+            Ok((_, packet)) => packet,
+            Err(e) => {
+                eprintln!("malformed packet: {e:?}");
+                continue;
+            }
+        };
+
+        if let Some(ref forwarder) = forwarder {
+            let mut len = 0;
+
+            while len < bytes.len() {
+                let Ok(i) = forwarder.send(&bytes[len..]).await else {
+                    continue 'conn;
+                };
+                len += i;
+            }
+
+            let Ok(_) = forwarder.recv_buf(&mut out_buf).await else {
+                continue 'conn;
+            };
+
+            udp_socket
+                .send_to(&out_buf, source)
+                .await
+                .expect("Failed to send response");
+        } else {
+            let packet = transform(packet);
+
+            packet.encode(&mut out_buf);
+
+            udp_socket
+                .send_to(&out_buf, source)
+                .await
+                .expect("Failed to send response");
         }
+
+        out_buf.clear();
     }
 }
 
